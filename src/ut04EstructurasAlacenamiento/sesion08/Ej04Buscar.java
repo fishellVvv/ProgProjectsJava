54a528b13b2d10e69c52549fa947b387df5f064d
fishellVvv <victorvigilvela@gmail.com>
2025-04-30 08:19:45 +0200

    correcciones

--- src/ut04EstructurasAlacenamiento/sesion08/Ej04Buscar.java
+++ src/ut04EstructurasAlacenamiento/sesion08/Ej04Buscar.java
@@ -1,6 +1,5 @@
 package ut04EstructurasAlacenamiento.sesion08;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ej04Buscar {
@@ -12,20 +11,21 @@ public class Ej04Buscar {
          */
 
         Scanner scn = new Scanner(System.in);
-        int[] numeros = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] numeros = new int[] { 45, 20, 31, 3, 78, 13, 90, 55, 89, 8 };
+        boolean encontrado = false;
 
-        System.out.println("Escribe un número del 1 al 10 para buscarlo: ");
+        System.out.print("Escribe un número para buscarlo: ");
         int numeroUsuario = scn.nextInt();
 
-        /*
-        for (int i=0; i<numeros.length; i++) {
-            if (numeros[i] == numeroUsuario) {
-                System.out.println("El número se encuentra en la posición " + i + " del array.");
+            for (int i=0; i<numeros.length; i++) {
+                if (numeros[i] == numeroUsuario) {
+                    encontrado = true;
+                    System.out.println("El número " + numeroUsuario + " se encuentra en la posición " + i + " del array.");
+                    break;
+                }
             }
-        }
-         */
 
-        System.out.println("El número se encuentra en la posición " + Arrays.binarySearch(numeros, numeroUsuario) + " del array.");
+        if (!encontrado) System.out.println("El número no se encuentra en el listado del array.");
 
         scn.close();
     }
