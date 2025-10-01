45d784d7fa10b6c0478bb614784178a8239efccf
fishellVvv <victorvigilvela@gmail.com>
2025-10-01 22:42:30 +0200

    pijaditas

--- src/ut05OrientacionObjetos/sesion14/PasarelaPago/PasarelaPago.java
+++ src/ut05OrientacionObjetos/sesion14/PasarelaPago/PasarelaPago.java
@@ -5,7 +5,7 @@ public class PasarelaPago {
         Pago pago1 = new PagoTarjeta(150, "1234-5678-1234-5678");
         Pago pago2 = new Paypal(300, "cliente@correo.com");
 
-        Pago[] pagos = { pago1, pago2};
+        Pago[] pagos = {pago1, pago2};
 
         for (Pago p: pagos){
             p.procesarPago();
