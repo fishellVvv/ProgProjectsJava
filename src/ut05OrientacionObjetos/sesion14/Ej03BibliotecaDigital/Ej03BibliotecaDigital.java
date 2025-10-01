0c2d05c175d018eb618d970edba8a3848f7c8830
fishellVvv <victorvigilvela@gmail.com>
2025-10-01 23:37:33 +0200

    añadidos enunciados

--- src/ut05OrientacionObjetos/sesion14/Ej03BibliotecaDigital/Ej03BibliotecaDigital.java
+++ src/ut05OrientacionObjetos/sesion14/Ej03BibliotecaDigital/Ej03BibliotecaDigital.java
@@ -1,5 +1,13 @@
 package ut05OrientacionObjetos.sesion14.Ej03BibliotecaDigital;
 
+// 3 – Biblioteca digital
+// Crea una clase Recurso con atributos título y autor.
+// Crea dos subclases:
+//         • Libro con atributo numeroPaginas.
+//         • Revista con atributo numeroEdicion.
+// En cada clase implementa un mét_odo mostrarInfo() que imprima todos los datos.
+// En el main, guarda vários Recurso en un array (Libro y Revista) y muestra la información de cada uno.
+
 public class Ej03BibliotecaDigital {
     public static void main(String[] args) {
         Recurso libro1 = new Libro("El Señor de los Anillos", "J. R. R. Tolkien", 1662);
